f837dc61e3e83fa46181065e93827b4950ba955a
Melisa Durmuş <melisa-1347@hotmail.com>
2022-08-16 17:40:20 +0300

    signin sayfası

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ParentPage from "./Pages/ParentPage";
-import Childscreen from "./Pages/Childscreen";
+import Signin from "./Pages/Signin";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/parent" element={<ParentPage />} />
-      <Route path="/Child" element={<Childscreen />} />
+      <Route path="/signin" element={<Signin />} />
     </Routes>
   );
 }
