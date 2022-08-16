16e5090aa390798a027e9f8f01e9a7da48e885c6
Bilal Murat <bilalmurat@users.noreply.github.com>
2022-08-16 11:47:07 +0300

    remove test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import HomePage2 from "./Pages/HomePage2";
 import ParentPage from "./Pages/ParentPage";
 
 
@@ -9,10 +8,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/" element={<HomePage2 />} />
-      <Route path="/" element={<HomePage2 />} />
-      <Route path="/" element={<HomePage2 />} />
-      <Route path="/" element={<HomePage2 />} />
       <Route path="/parent" element={<ParentPage />} />
     </Routes>
   );
