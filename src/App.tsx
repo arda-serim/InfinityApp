44dd2a139af10a07754980b9bb77e6c19cbbc5d2
Melisa Durmuş <melisa-1347@hotmail.com>
2022-08-16 11:56:44 +0300

    merhaba

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ParentPage from "./Pages/ParentPage";
-import Childcsreen from "./Pages/Childscreen";
+import Childscreen from "./Pages/Childscreen";
 
 
 function App() {
