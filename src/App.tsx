edf36f0640916ed85b4fffd4c64a4b4b0209f9dc
Bilal Murat <bilalmurat@users.noreply.github.com>
2022-08-16 11:37:19 +0300

    appp update

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/parent" element={<ParentPage />} />
     </Routes>
   );
