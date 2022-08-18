c31cb1a19d77c63a7313ba54cc4d81505a17dbf1
Melisa Durmuş <melisa-1347@hotmail.com>
2022-08-18 13:53:22 +0300

    bot deneme

--- src/Pages/Signin.tsx
+++ src/Pages/Signin.tsx
@@ -1,3 +1,4 @@
+
 import { Layout  , Card, Checkbox, Button,Radio , Typography , Input, AutoComplete} from 'antd';
 import React from 'react';
 import Navbar from '../Components/Navbar';
@@ -10,6 +11,8 @@ import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 import logo from '../images/logo_infinity.png';
 import { LoginOutlined  } from '@ant-design/icons';
 import type { SizeType } from 'antd/es/config-provider/SizeContext';
+import LangContext, {langs} from './LangContext';
+
 
 
 const { Title } = Typography;
@@ -53,8 +56,7 @@ const inputStyle = {
 const signInButtonStyle = {
   width: '350px',
   height: '40px',
-  //background: 'linear-gradient( #327FA3 , #1D1B65)',
-  background: 'linear-gradient( %41.67,#327FA3,%100,#1D1B65)',
+  background: 'linear-gradient(180deg, #327FA3 41.67%, #1D1B65 100%)',
   color: '#fff',
   border: 'none',
 };
@@ -88,7 +90,17 @@ const { Header, Content } = Layout;
 
 function Signin(){
  
+  const [lang,setLang] = useState(langs.tr)
+  const [selectedLanguage , setSelectedLanguage] = useState("tr")
+
+  const switchLang=()=>{
+    setSelectedLanguage(selectedLanguage==="tr" ? "en" : "tr");
+    lang===langs.tr ? setLang(langs.en):setLang(langs.tr)
+  }
+
   return(
+
+    <LangContext.Provider value={lang}>
 
     <Layout style={{ backgroundPosition: 'center',
     backgroundSize: 'cover',
@@ -98,6 +110,7 @@ function Signin(){
       <Header style={navbarStyle}>
          <img src={logo} alt="logo" style={logoStyle} />
          <h1 style={titleStyle}>Infinity</h1>
+         <button style={{marginLeft : '80%' , background: 'rgba(0,0,0,0)', border : 'none'   }} onClick={switchLang} > Dil Değiştir </button>
 
       </Header >
         <div style={contentStyle}>
@@ -112,17 +125,17 @@ function Signin(){
                   </br>
                   <div style={whitePlaceStyle}>
                     <Title style={{color: '#0A103A', marginTop:'25'
-                    }}>LET'S YOU SIGN IN</Title>
+                    }}>{lang.giris}</Title>
                    </div>
                    <div style={whitePlaceStyle}> 
-                    <Title level={5} style={{color: '#0A103A', marginRight: '160px'}}>Welcome to our page</Title>
+                    <Title level={5} style={{color: '#0A103A', marginRight: '160px'}}>{lang.hosgeldin}</Title>
                   </div>
                   <br>
                   </br>
                   <br>
                   </br>
                   <div style={whitePlaceStyle}>
-                    <Title level ={2} style={{color: '#0A103A', marginRight: '230px'}}>Name</Title>,
+                    <Title level ={2} style={{color: '#0A103A', marginRight: '230px'}}>{lang.ad}</Title>
                   </div>
                   <div style={whitePlaceStyle}>
                     <Input style={inputStyle}/>
@@ -130,7 +143,7 @@ function Signin(){
                   <br>
                   </br>
                   <div style={whitePlaceStyle}>
-                    <Title level ={2} style={{color: '#0A103A',  marginRight: '200px'}}>Surname</Title>
+                    <Title level ={2} style={{color: '#0A103A',  marginRight: '200px'}}>{lang.soyad}</Title>
                   </div>
                   <div style={whitePlaceStyle}>
                     <Input style={inputStyle}/>
@@ -138,13 +151,13 @@ function Signin(){
                   <br>
                   </br>
                   <div style={whitePlaceStyle}>
-                    <Checkbox onChange={onChange} style={{marginRight: '170px'}} >Keep me logged in</Checkbox>;
+                    <Checkbox onChange={onChange} style={{marginRight: '170px'}} >{lang.checkbox}</Checkbox>
                   </div>
                   <br>
                   </br>
                   <div style={whitePlaceStyle}>
                     <Button size="large" style={signInButtonStyle} shape='round'>
-                    Sıgn In</Button>
+                    {lang.kayıtol}</Button>
                   </div>
                 </Col>
               </Row>
@@ -152,6 +165,8 @@ function Signin(){
           </Content>
         </div>
     </Layout>
+   
+    </LangContext.Provider>
   );
 }
 
