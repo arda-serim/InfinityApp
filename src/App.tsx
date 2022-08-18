8789ac3ba3521f251d159c420392964ca2c9fdcb
Melisa Durmuş <melisa-1347@hotmail.com>
2022-08-18 10:16:55 +0300

    childedit değişecek

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Childedit from "./Pages/Childedit";
 import HomePage from "./Pages/HomePage";
 import ParentPage from "./Pages/ParentPage";
 import Signin from "./Pages/Signin";
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/parent" element={<ParentPage />} />
       <Route path="/signin" element={<Signin />} />
+      <Route path="/childedit" element={<Childedit />} />
     </Routes>
   );
 }
